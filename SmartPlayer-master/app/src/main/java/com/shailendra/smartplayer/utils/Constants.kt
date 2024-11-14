ffcfbb1365a7716b60bbf44c1ac4a6e8783e7ef0
shailendrapokhriyal96 <62931606+shailendrapokhriyal96@users.noreply.github.com>
2024-11-14 15:14:44 +0530

    Update Constants.kt

--- SmartPlayer-master/app/src/main/java/com/shailendra/smartplayer/utils/Constants.kt
+++ SmartPlayer-master/app/src/main/java/com/shailendra/smartplayer/utils/Constants.kt
@@ -2,6 +2,6 @@ package com.shailendra.smartplayer.utils
 
 object Constants {
     const val VIDEO_NAME = "Demo-Video"
-    const val VIDEO_URL = "http://d17wvyxcudl5jf.cloudfront.net/amritaTV/content/a_vg036tah/a.m3u8"
-    const val MATOMO_TRACKER_URL = "https://analyticstest.planetcast.in/matomo.php" //Live
+    const val VIDEO_URL = "http://sample.vodobox.net/skate_phantom_flex_4k/skate_phantom_flex_4k.m3u8"
+    const val MATOMO_TRACKER_URL = "https://test/matomo.php" //Live
 }
